b41180026edfcd26c5183f2162cfc3a5933fc7ec
Adrien <a.bourgois@student.isartdigital.com>
2017-03-01 11:54:37 +0100

    [Ini] Simple test of file parse

--- EngineTest/Source.cpp
+++ EngineTest/Source.cpp
@@ -1,7 +1,12 @@
 #include "Windows.h"
+#include "Tools/Ini/IniParser.h"
 
 int WINAPI wWinMain(HINSTANCE _hInstance, HINSTANCE _hPrevInstance, PWSTR _pCmdLine, int _nCmdShow)
 {
+	IniParser* parser = new IniParser("config.ini");
+	bool result = parser->Parse();
+	float value = parser->GetFloat("Values", "life");
+	delete parser;
 
 	return EXIT_SUCCESS;
 }
